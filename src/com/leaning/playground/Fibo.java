c905b008ef8e4e6922d3074a246a1c771be532e8
Avinash Vundyala <avinash.v@accoliteindia.com>
2017-05-12 03:08:08 +0530

    testing

--- src/com/leaning/playground/Fibo.java
+++ src/com/leaning/playground/Fibo.java
@@ -6,6 +6,7 @@ package com.leaning.playground;
 public class Fibo {
     public static void main(String args[]) {
         int n1 = 0, n2 = 1, n3, i, count = 10;
+        int a = 0;
         System.out.print(n1+" "+n2);
         for(i = 2; i < count; ++i) {
             n3=n1+n2;
